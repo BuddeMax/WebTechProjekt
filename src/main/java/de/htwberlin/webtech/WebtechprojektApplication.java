b3ded81e74a7d7bf691dd34f44f80212d2dbd1d1
BuddeMax <Max.Budde@Student.HTW-Berlin.de>
2023-10-14 21:43:48 +0200

    Hello World

---
+++ src/main/java/de/htwberlin/webtech/WebtechprojektApplication.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechprojektApplication.class, args);
+	}
+
+}
