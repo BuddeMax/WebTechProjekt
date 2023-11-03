01f0b842cd24488e2617ac75b353779333b798d5
BuddeMax <Max.Budde@Student.HTW-Berlin.de>
2023-11-03 07:42:06 +0100

    Milestone 2

---
+++ src/main/java/de/htwberlin/webtech/web/ThingRepository.java
@@ -0,0 +1,8 @@
+package de.htwberlin.webtech.web;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThingRepository extends CrudRepository<Thing, Long> { }
